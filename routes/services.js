cdc0afe2d6dc3e7a5d8398af9112dac6024e6d57
Derick Gross <derick.gross@gmail.com>
2020-04-08 00:08:49 -0400

    extract connection logic from routes

--- routes/services.js
+++ routes/services.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var router = express.Router();
+
 const redis = require('redis');
 const PORT = process.env.REDIS_PORT;
 const HOST = process.env.REDIS_IP;
+
 const client = redis.createClient(PORT, HOST);
+const connection = new Promise((resolve, reject) => {
+  const client = redis.createClient(PORT, HOST);
+
+  client.on('connect', () => {
+    resolve(client);
+  });
+
+  client.on('error', (err) => {
+    console.log('An error occurred while connecting to the config store:');
+    console.error(err);
+    console.log('\n');
+  });
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	new Promise((resolve, reject) => {
-    const client = redis.createClient(PORT, HOST);
-
-    client.on('connect', () => {
-      resolve(client);
-    });
-
-    client.on('error', (err) => {
-      console.log('An error occurred while connecting to the config store:');
-      console.error(err);
-      console.log('\n');
-    });
-  })
-  .then((client) => {
+  connection.then((client) => {
     client.hgetall('service-hosts', (err, services) => {
     	res.send(services);
     });
@@ -28,43 +30,9 @@ router.get('/', function(req, res, next) {
   .catch(e => console.log('An error occurred: ', e));
 });
 
-router.get('/test', function(req, res, next) {
-	new Promise((resolve, reject) => {
-    const client = redis.createClient(PORT, HOST);
-
-    client.on('connect', () => {
-      resolve(client);
-    });
-
-    client.on('error', (err) => {
-      console.log('An error occurred while connecting to the config store:');
-      console.error(err);
-      console.log('\n');
-    });
-  })
-  .then((client) => {
-    client.hgetall(`postman:cnn`, (err, config) => {
-      res.send(config);
-    });
-  })
-  .catch(e => console.log('An error occurred: ', e));
-});
-
+// GET config for requesting/responding service combination
 router.get('/:reqService/:resService', function(req, res, next) {
-	new Promise((resolve, reject) => {
-    const client = redis.createClient(PORT, HOST);
-
-    client.on('connect', () => {
-      resolve(client);
-    });
-
-    client.on('error', (err) => {
-      console.log('An error occurred while connecting to the config store:');
-      console.error(err);
-      console.log('\n');
-    });
-  })
-  .then((client) => {
+	connection.then((client) => {
     client.hgetall(`${req.params["reqService"]}:${req.params["resService"]}`, (err, config) => {
       res.send(config);
     });
