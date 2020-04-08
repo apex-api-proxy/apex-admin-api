5c11b78f54fee559e9a6594d483b40849f9d17ce
Derick Gross <derick.gross@gmail.com>
2020-04-08 17:50:12 -0400

    add CORS headers

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.use('/services', servicesRouter);
 app.use('/config', configRouter);
 // app.use('/users', usersRouter);
 
+// Set CORS headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
