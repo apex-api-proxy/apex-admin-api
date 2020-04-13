d3e9d084bddf495b5ba46e49bc7d58d338226175
Derick Gross <derick.gross@gmail.com>
2020-04-12 23:24:30 -0400

    configure logs client with environment variables

--- routes/logs.js
+++ routes/logs.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { Client } = require('pg');
-const client = new Client();
+const client = new Client({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+})
 
 const text = 'SELECT * FROM apex_logs WHERE correlation_id = $1';
 
